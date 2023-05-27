9f61f55522d792e45f09e56093bf210a8d8fafb0
Jad229 <jad229@njit.edu>
2023-05-27 19:35:10 -0400

    first commit

---
+++ dist/index.bundle.js
@@ -0,0 +1,22 @@
+(self["webpackChunkadventure_cafe"] = self["webpackChunkadventure_cafe"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+console.log("Successfully connected");
+
+alert("Script LOADED!");
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUE7QUFDQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vYWR2ZW50dXJlLWNhZmUvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJTdWNjZXNzZnVsbHkgY29ubmVjdGVkXCIpO1xyXG5cclxuYWxlcnQoXCJTY3JpcHQgTE9BREVEIVwiKTtcclxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
---
+++ src/index.js
@@ -0,0 +1,3 @@
+console.log("Successfully connected");
+
+alert("Script LOADED!");
